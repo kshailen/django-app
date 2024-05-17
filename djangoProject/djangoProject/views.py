efc29bfdd1731afe5d82324fa2d38eebd84125fd
Shailendra Kumar <emailtoshailendra@gmal.com>
2024-05-17 17:53:25 +0530

    Adding static files css

--- djangoProject/djangoProject/views.py
+++ djangoProject/djangoProject/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, world. You are at Home")
+    return render(request, 'website/index.html')
+
 
 def about(request):
     return HttpResponse("Hello, world. You are at About Page")
