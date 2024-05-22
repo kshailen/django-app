4c1ba078873dab27815e98bc09870c9b011228ae
Shailendra Kumar <emailtoshailendra@gmal.com>
2024-05-22 19:37:36 +0000

    Adding tailwind css

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('greno', include('greno.urls')),
+
+
+    path("_reload_/", include('django_browser_reload.urls'))
 ]
